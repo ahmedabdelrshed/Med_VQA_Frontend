86bd92ca380685218445f937343d03f805fc1257
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-02-22 23:06:52 +0200

    initial version

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+createRoot(document.getElementById('root')!).render(
+  <>
+    <div className="bg-blue-400 text-4xl text-center text-white w-fit m-auto p-10 px-8 mt-52 rounded-2xl">
+      <h1>Medical VQA App</h1>
+    </div>
+  </>
+)
