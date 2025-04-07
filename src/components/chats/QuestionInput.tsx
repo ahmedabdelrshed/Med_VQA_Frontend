5f632d84d873a79a101a8d54fd74be4ea4b24840
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-04-07 07:56:30 +0200

    send button

--- src/components/chats/QuestionInput.tsx
+++ src/components/chats/QuestionInput.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { CiImageOn } from "react-icons/ci";
 import { IoMdAddCircleOutline } from "react-icons/io";
 import { MdCancel } from "react-icons/md";
+import { LuSend } from "react-icons/lu";
 const QuestionInput = () => {
   // Automatically focus the input field when the page loads or is refreshed
   window.onload = () => {
@@ -27,7 +28,7 @@ const QuestionInput = () => {
     setSelectedImage(null);
   };
   return (
-    <div className="absolute  border-3 border-[#6178ff] rounded-xl  bottom-3  lg:bottom-25 left-1/2 transform -translate-x-1/2 bg-white px-4 py-4 shadow-lg w-[90%] max-w-xl ">
+    <div className="absolute  border-3 border-[#6178ff] rounded-xl  bottom-3  lg:bottom-16 left-1/2 transform -translate-x-1/2 bg-white px-4 py-4 shadow-lg w-[90%] max-w-xl ">
       <input
         id="myInput"
         type="text"
@@ -42,27 +43,32 @@ const QuestionInput = () => {
         style={{ display: "none" }}
         onChange={handleUploadImage}
       />
-      {selectedImage ? (
-        <div className="relative px-1 ml-3 mt-2 bg-gray-300 rounded-md py-1 cursor-pointer w-fit ">
-          <img src={selectedImage} className="w-16 rounded-md" alt="" />
-          <button
-            className="absolute text-white top-[-2px] cursor-pointer right-[-5px] bg-gray-500 hover:bg-red-600 rounded-full p-1"
-            onClick={onCancelUpload}
+      <div className="flex justify-between items-center ">
+        {selectedImage ? (
+          <div className="relative px-1 ml-3 mt-2 bg-gray-300 rounded-md py-1 cursor-pointer w-fit ">
+            <img src={selectedImage} className="w-16 rounded-md" alt="" />
+            <button
+              className="absolute text-white top-[-2px] cursor-pointer right-[-5px] bg-gray-500 hover:bg-red-600 rounded-full p-1"
+              onClick={onCancelUpload}
+            >
+              <MdCancel className="w-3 h-3" />
+            </button>
+          </div>
+        ) : (
+          <div
+            className="flex px-4 ml-3 bg-gray-300 rounded-xl py-1 cursor-pointer w-fit text-sm items-center mt-5"
+            onClick={() => {
+              document.getElementById("inputImage")?.click();
+            }}
           >
-            <MdCancel className="w-3 h-3" />
-          </button>
-        </div>
-      ) : (
-        <div
-          className="flex px-4 ml-3 bg-gray-300 rounded-xl py-1 cursor-pointer w-fit text-sm items-center mt-5"
-          onClick={() => {
-            document.getElementById("inputImage")?.click();
-          }}
-        >
-          <IoMdAddCircleOutline className="w-5.5 h-5.5 text-gray-700 mr-3 bg-gray-400 rounded-2xl p-1" />{" "}
-          Image <CiImageOn className="w-4 h-4 ml-1 mt-[3px]" />
-        </div>
-      )}
+            <IoMdAddCircleOutline className="w-5.5 h-5.5 text-gray-700 mr-3 bg-gray-400 rounded-2xl p-1" />{" "}
+            Image <CiImageOn className="w-4 h-4 ml-1 mt-[3px]" />
+          </div>
+        )}
+        <button className="mt-4 mr-3 text-white gradient-bg cursor-pointer p-2 rounded-full">
+          <LuSend className="w-4 h-4" />
+        </button>
+      </div>
     </div>
   );
 };
