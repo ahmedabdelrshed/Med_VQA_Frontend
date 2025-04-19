7d96ffc56a4487c1ebcbf2ca84b2c68e877bd482
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-04-19 10:34:35 +0200

    handle empty chat

--- src/components/chats/ChatInterface.tsx
+++ src/components/chats/ChatInterface.tsx
@@ -2,15 +2,19 @@ import { useParams } from "react-router";
 import QuestionForm from "./QuestionForm";
 import QuestionsList from "./QuestionsList";
 import { useGetQuestionsQuery } from "../../store/questions/questionsApi";
-// import WelcomeMessage from "./WelcomeMessage";
+import WelcomeMessage from "./WelcomeMessage";
 
 const ChatInterface = () => {
   const { id } = useParams();
   const { data: questions } = useGetQuestionsQuery(id as string);
   return (
     <div className="h-[80vh] lg:h-screen relative  z-0">
-      {questions && <QuestionsList Questions={questions.data} />}
-      {/* <WelcomeMessage /> */}
+      {questions?.data.length ? (
+        <QuestionsList Questions={questions.data} />
+      ) : (
+        <WelcomeMessage />
+      )}
+
       <QuestionForm />
     </div>
   );
