c9043770bbb668d39d1b752ca1cb97fd67e819dd
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-05-01 18:52:37 +0300

    handle join us

--- src/pages/ShareChat.tsx
+++ src/pages/ShareChat.tsx
@@ -3,13 +3,13 @@ import getSharedChat from "../hooks/useGetSharedChat";
 import { TQuestion } from "../Types";
 import RenderQuestionSkeleton from "../components/questions/RenderQuestionSkeleton";
 import QuestionsList from "../components/questions/QuestionsList";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 
 const ShareChat = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [questions, setQuestions] = useState<TQuestion[] | null>(null);
   const { id } = useParams();
-
+  const navigate = useNavigate();
   useEffect(() => {
     if (id) getSharedChat({ setIsLoading, setQuestions, id });
   }, [id]);
@@ -18,7 +18,10 @@ const ShareChat = () => {
     <div>
       <header className="flex items-center bg-gray-300 px-5 py-2 justify-between lg:px-20 lg:py-3">
         <img src="/images/logo.png" className="w-30 lg:w-38 " alt="" />
-        <button className="bg-white cursor-pointer border py-2 px-6 w-fit  rounded-lg font-medium border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out">
+        <button
+          className="bg-white cursor-pointer border py-2 px-6 w-fit  rounded-lg font-medium border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out"
+          onClick={() => navigate("/")}
+        >
           Join US
         </button>
       </header>
