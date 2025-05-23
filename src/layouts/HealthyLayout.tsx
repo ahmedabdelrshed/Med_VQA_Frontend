57fbaf7600bd9c96294f842e93fc856ea8dfdafb
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-05-23 17:14:16 +0300

    update healthy navbar  profile page

--- src/layouts/HealthyLayout.tsx
+++ src/layouts/HealthyLayout.tsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router";
 import SideBar from "../components/HealthyProfile/SideBar";
+import { GrMenu } from "react-icons/gr";
 
 const HealthyLayout = () => {
   return (
-    <div className="bg-[#f1f5f9] flex  overflow-y-hidden">
+    <div className="bg-[#f1f5f9] lg:flex  overflow-y-hidden">
+      <div className=" bg-white  lg:hidden  pb-2 pt-3 px-6 flex justify-between">
+        <label htmlFor="health-drawer">
+          <GrMenu className="w-7 h-7 p-1    cursor-pointer" />
+        </label>
+        <img src="/images/logo.png" className="w-34" alt="" />
+        <div />
+      </div>
       <SideBar />
       <div className="flex-1">
         <Outlet />
